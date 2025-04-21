49b075d4b910de7599e60aa483dcb9ffb18f7e39
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-21 10:20:42 +0500

    Implement dynamic rendering based on user role

---
+++ CLIENT/src/components/Headers/Header.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import HeaderUser from "./HeaderUser";
+import HeaderSuperAdmin from "./HeaderSuperAdmin";
+import HeaderLabAdmin from "./HeaderLabAdmin";
+import HeaderGuest from "./HeaderGuest";
+import Topbar from "../Layouts/Topbar";
+
+const Header = () => {
+  const user = useSelector((state) => state.Auth?.user);
+  const role = user?.role?.toLowerCase()?.replace(/\s+/g, '');
+
+  if (!user) return <HeaderGuest />;
+
+  if (role === "superadmin") {
+    return <HeaderSuperAdmin />;
+  }
+
+  if (role === "labadmin") {
+    return <HeaderLabAdmin />;
+  }
+
+  return (
+    <>
+      <Topbar />
+      <HeaderUser />
+    </>
+  );
+};
+
+export default Header;
