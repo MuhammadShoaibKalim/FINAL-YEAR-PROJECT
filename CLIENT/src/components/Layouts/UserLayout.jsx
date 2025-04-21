3535ef7812d55db1e236ea96c563ea457ad19b33
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-21 10:24:47 +0500

    Update UserLayout to import Header from the correct path and comment out the previous import statement

--- CLIENT/src/components/Layouts/UserLayout.jsx
+++ CLIENT/src/components/Layouts/UserLayout.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
-import Header from "../Layouts/Header";
+// import Header from "../Layouts/Header";
 import Footer from "../Common/Footer";
+import Header from  "../Headers/Header"
 
 const UserLayout = () => {
   const { pathname } = useLocation(); 
