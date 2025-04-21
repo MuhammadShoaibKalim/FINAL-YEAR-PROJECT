ce19db62bad7dfb5c791e8b8edbfabd60a6a960b
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-21 10:19:15 +0500

    Update API proxy target and enable secure connection

--- CLIENT/vite.config.js
+++ CLIENT/vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
   server: {
   proxy: {
     '/api': {
-      target: 'http://localhost:5000',
+      target: 'http://localhost:7000',
       changeOrigin: true,
-      secure: false,
+      secure: true,
     },
   },
 },
