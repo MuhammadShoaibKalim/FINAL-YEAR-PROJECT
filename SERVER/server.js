c084b68d3e46170c728fb83e8d86d917e451a048
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-23 01:58:46 +0500

    Add static file serving for uploads in server configuration

--- SERVER/server.js
+++ SERVER/server.js
@@ -28,6 +28,8 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads'));
+
 
 //Routes
 app.use("/api/auth", authRouter);  
