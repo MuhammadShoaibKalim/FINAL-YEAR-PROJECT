34826804cfc24479a8036009f655a62e30e08052
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-23 18:18:46 +0500

    Enhance lab creation route to include image upload functionality

--- SERVER/routes/lab.route.js
+++ SERVER/routes/lab.route.js
@@ -8,10 +8,12 @@ import {
   getLab,
 } from "../controllers/lab.controller.js";
 import {  isAuthenticated, isLabAdmin, isSuperAdmin } from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/upload.middleware.js";
 
 const router = express.Router();
 
-router.post("/create", isAuthenticated, isSuperAdmin , addLab);
+router.post("/create", isAuthenticated, isSuperAdmin, upload.single("image"), addLab);
+// router.post("/create", isAuthenticated, isSuperAdmin , addLab);
 router.get("/", isAuthenticated, isLabAdmin, getLab);
 // router.get("/:id", isAuthenticated, isSuperAdmin, isLabAdmin, getLabById);
 router.put("/:id", isAuthenticated, isLabAdmin, updateLab);
