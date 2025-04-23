7033285d9d0e261b7f9c2e6834061e0bd0d697b5
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-23 18:16:06 +0500

    Enhance token generation in auth utility

--- SERVER/utils/auth.util.js
+++ SERVER/utils/auth.util.js
@@ -1,17 +1,21 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
-    if (!process.env.SECRET_KEY) {
-        throw new Error("Secret key is not defined in environment variables");
-    }
+  if (!process.env.SECRET_KEY) {
+    throw new Error("Secret key is not defined in environment variables");
+  }
 
-    try {
-        return jwt.sign(
-            { id: user._id, role: user.role  },
-              process.env.SECRET_KEY,
-            { expiresIn: 3 * 24 * 60 * 60 }
-        );
-    } catch (error) {
-        throw new Error("Error generating token: " + error.message);
-    }
+  try {
+    return jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+        email: user.email, 
+      },
+      process.env.SECRET_KEY,
+      { expiresIn: "7d" } 
+    );
+  } catch (error) {
+    throw new Error("Error generating token: " + error.message);
+  }
 };
