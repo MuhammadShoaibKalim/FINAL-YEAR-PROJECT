7733d1a899a11a2d9a2e90469faa35661f23e4f2
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-23 18:15:23 +0500

    Refactor server.js to improve middleware configuration and ensure consistent formatting.

--- SERVER/server.js
+++ SERVER/server.js
@@ -25,8 +25,8 @@ const app = express();
 
 app.use(cookieParser())
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
 
