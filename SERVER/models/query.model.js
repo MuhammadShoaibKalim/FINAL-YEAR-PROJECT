69e533a29054760e5afbc3334b651fd7f0a84b04
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-24 18:02:44 +0500

    Update user reference capitalization in query model from "user" to "User" for consistency and adherence to naming conventions.

--- SERVER/models/query.model.js
+++ SERVER/models/query.model.js
@@ -11,7 +11,7 @@ const querySchema = new mongoose.Schema({
   },
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
+    ref: "User",
     required: false, 
   },
   subject: {
