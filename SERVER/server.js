197466d64aa2e7b6c696b778b0c0fcb79b23dfe4
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-24 17:48:05 +0500

    Refactor server routes to consolidate admin and lab admin routes under a single admin route, and update cart route to use order routes instead.

--- SERVER/server.js
+++ SERVER/server.js
@@ -3,16 +3,13 @@ import connectDB from './config/db.js';
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import authRouter from "./routes/auth.route.js";
 import orderRoutes from "./routes/order.route.js";
-import superAdminRoutes from "./routes/superAdmin.route.js";
-import labAdminRoutes from "./routes/labAdmin.route.js";
+import adminRoutes from "./routes/admin.route.js";
 import {protect} from './middlewares/auth.middleware.js';
 import labRoutes from "./routes/lab.route.js";
 import userRoutes from "./routes/user.route.js";
 import queryRoutes from "./routes/query.route.js"
 import aiRoutes from "./routes/ai.route.js"
-import cartRoutes from "./routes/cart.route.js"
 import paymentRoutes from "./routes/payment.route.js"
 import testPackageRoutes from "./routes/testpackage.route.js"
 import cors from "cors";
@@ -32,16 +29,16 @@ app.use('/uploads', express.static('uploads'));
 
 
 //Routes
-app.use("/api/auth", authRouter);  
-app.use("/api/superadmin", superAdminRoutes);
-app.use("/api/labadmin", labAdminRoutes);
+app.use("/api/auth", userRoutes);  
+app.use("/api/superadmin", adminRoutes);
+app.use("/api/labadmin", adminRoutes);
 app.use("/api/labs",  labRoutes);
 app.use("/api/tests", testPackageRoutes );
 app.use("/api/packages", testPackageRoutes );
 app.use("/api/users", userRoutes)
 app.use("/api/query", queryRoutes);
 app.use("/api/ai", aiRoutes)
-app.use("/api/cart", cartRoutes);
+app.use("/api/cart", orderRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/payment", paymentRoutes);
 
