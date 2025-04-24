c6a3373b01f6ff63f55f1bd6eb1fd20fb61715e0
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-24 18:03:40 +0500

    Add user routes in user.route.js for registration, login, logout, and profile management, enhancing user authentication and account management functionalities.

--- SERVER/routes/user.route.js
+++ SERVER/routes/user.route.js
@@ -1,13 +1,39 @@
 import express from "express";
+import { 
+    userRegister, 
+    userLogin, 
+    userLogout, 
+    getUserProfile, 
+    updateUserProfile, 
+    deleteUserAccount, 
+    checkUser} from "../controllers/user.controller.js";
 import { isAuthenticated, isSuperAdmin } from "../middlewares/auth.middleware.js";
 import {
   getUsers, 
   updateUser,
   deleteUser,
 } from "../controllers/user.controller.js";
-import { createLabAdmin } from "../controllers/superAdmin.controller.js";
+import { createLabAdmin } from "../controllers/admin.controller.js";
+import upload from '../middlewares/upload.middleware.js';
+
+
 
 const router = express.Router();
+
+//  Public Routes
+router.post("/register", userRegister);
+router.post("/login", userLogin);
+router.post("/logout", userLogout);
+
+// Protected Routes (Require Authentication)
+router.get("/profile", isAuthenticated, getUserProfile);
+router.put('/profile/:id', isAuthenticated, upload.single('image'), updateUserProfile);
+router.get("/getuser", isAuthenticated, checkUser);
+router.delete("/:id", isAuthenticated, deleteUserAccount);
+
+
+
+
 
 // Super Admin Creates Lab Admin(Users)
 router.post("/create-labadmin", isAuthenticated, isSuperAdmin, createLabAdmin);
