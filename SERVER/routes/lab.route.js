758699f5de2605b97cb13255a88f87db2c747fc4
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-24 18:03:22 +0500

    Implement GET route for retrieving all labs and update existing GET route to restrict access to Super Admins. This enhances lab management capabilities by allowing comprehensive lab data retrieval.

--- SERVER/routes/lab.route.js
+++ SERVER/routes/lab.route.js
@@ -4,8 +4,8 @@ import {
   updateLab,
   deleteLab,
   // getLabById,
-  // getAllLabs,
   getLab,
+  getAllLabs,
 } from "../controllers/lab.controller.js";
 import {  isAuthenticated, isLabAdmin, isSuperAdmin } from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/upload.middleware.js";
@@ -14,7 +14,8 @@ const router = express.Router();
 
 router.post("/create", isAuthenticated, isSuperAdmin, upload.single("image"), addLab);
 // router.post("/create", isAuthenticated, isSuperAdmin , addLab);
-router.get("/", isAuthenticated, isLabAdmin, getLab);
+router.get("/", isAuthenticated, isSuperAdmin, getLab);
+router.get("/all", isAuthenticated, isSuperAdmin, getAllLabs);
 // router.get("/:id", isAuthenticated, isSuperAdmin, isLabAdmin, getLabById);
 router.put("/:id", isAuthenticated, isLabAdmin, updateLab);
 router.delete("/:id", isAuthenticated,isLabAdmin, deleteLab);
