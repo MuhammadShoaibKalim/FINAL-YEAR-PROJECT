d3d150fec55af69305abad690ef53a1c7c91081c
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-28 23:50:59 +0500

    Refactor query schema to include trimming and lowercase options for string fields, and add timestamps for better record management.

--- SERVER/models/query.model.js
+++ SERVER/models/query.model.js
@@ -1,48 +1,56 @@
 import mongoose from "mongoose";
 
-const querySchema = new mongoose.Schema({
-  name: { 
-    type: String,
-    required: true
+const querySchema = new mongoose.Schema(
+  {
+    name: { 
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: { 
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    receiverType: {
+      type: String,
+      enum: ["support", "labadmin"],
+      default: "support",
+    },
+    labId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Lab",
+    },
+    subject: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: { 
+      type: String, 
+      required: true,
+      trim: true,
+    },
+    response: {
+      type: String,
+      default: null,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["unviewed", "viewed", "responded"],
+      default: "unviewed",
+    },
   },
-  email: { 
-    type: String,
-    required: true,
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: false, 
-  },
-  receiverType: {
-    type: String,
-    enum: ["support", "labadmin"],
-    default: "support",
-  },
-  labId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Lab",
-    required: false, 
-  },
-  subject: {
-    type: String,
-    required: true,
-  },
-  message: { 
-    type: String, 
-    required: true
-  },
-  response: {
-    type: String,
-    default: null,
-  },
-  status: {
-    type: String,
-    enum: ["unviewed", "viewed", "responded"],
-    default: "unviewed",
-  },
-  createdAt: { type: Date, default: Date.now },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Query = mongoose.model("Query", querySchema);
 export default Query;
