ddbabab5593051b2fb005e12d6eaee8d76e26387
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-30 00:44:23 +0500

    Refactor token generation to streamline payload creation and include labId for labadmin role

--- SERVER/utils/auth.util.js
+++ SERVER/utils/auth.util.js
@@ -1,20 +1,21 @@
 import jwt from "jsonwebtoken";
-
 export const generateToken = (user) => {
   if (!process.env.SECRET_KEY) {
     throw new Error("Secret key is not defined in environment variables");
   }
 
+  const payload = {
+    id: user._id,
+    role: user.role,
+    email: user.email,
+  };
+
+  if (user.role === "labadmin" && user.labId) {
+    payload.lab = user.labId; 
+  }
+
   try {
-    return jwt.sign(
-      {
-        id: user._id,
-        role: user.role,
-        email: user.email, 
-      },
-      process.env.SECRET_KEY,
-      { expiresIn: "7d" } 
-    );
+    return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "7d" });
   } catch (error) {
     throw new Error("Error generating token: " + error.message);
   }
