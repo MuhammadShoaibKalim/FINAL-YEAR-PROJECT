058c404c8f6afeb19a0e1a9dc00f325b34af3643
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-05-01 16:10:48 +0500

    Implement authentication middleware for checkout session route to ensure only authenticated users can create a checkout session.

--- SERVER/routes/payment.route.js
+++ SERVER/routes/payment.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { checkoutSession } from "../controllers/payment.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
-router.post("/create-checkout-session", checkoutSession);
+router.post("/create-checkout-session",isAuthenticated ,checkoutSession);
 export default router;
