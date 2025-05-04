62cf9785aafbe377b1dfd7d05d37a03c4cfc625a
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-05-04 20:29:05 +0500

    Refactor UserProfileLayout component to integrate Topbar and HeaderUser, enhancing the layout and user navigation experience.

--- CLIENT/src/components/UserAdmin/UserProfileLayout.jsx
+++ CLIENT/src/components/UserAdmin/UserProfileLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import UserSidebar from './UserSidebar';
-import Header from '../Layouts/Header';
 import Footer from '../Common/Footer';
+import Topbar from "../Layouts/Topbar";
+import HeaderUser from "../Headers/HeaderUser";
 
 const UserProfileLayout = () => {
   return (
     <>
-      <Header />
+      <Topbar/>
+      <HeaderUser/>
       <div className="w-full min-h-screen bg-gray-100 pt-6">
         <div className="max-w-6xl mx-auto bg-white rounded-xl shadow-md p-4 flex flex-col md:flex-row gap-6">
           <UserSidebar />
