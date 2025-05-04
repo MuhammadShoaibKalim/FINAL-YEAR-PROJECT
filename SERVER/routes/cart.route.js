fd27a656dc8ebd3781405c52b2e2a04a8d68750a
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-05-04 20:31:17 +0500

    Add cart route definitions to handle cart operations including adding, removing, and retrieving items, with authentication middleware for secure access.

---
+++ SERVER/routes/cart.route.js
@@ -0,0 +1,22 @@
+// ✅ routes/cart.routes.js
+import express from "express";
+import {
+  addToCart,
+  removeFromCart,
+  getUserCart,
+  getCartItem,
+  clearCart,
+} from "../controllers/cart.controller.js";
+
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+// Cart routes
+router.post("/add", isAuthenticated, addToCart);
+router.get("/", isAuthenticated, getUserCart);
+router.get("/:id", isAuthenticated, getCartItem);
+router.delete("/remove/:id", isAuthenticated, removeFromCart);
+router.delete("/clear", isAuthenticated, clearCart);
+
+export default router;
