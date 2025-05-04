2bf216169b56337ad529f1756946bd6b45132370
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-05-04 20:30:44 +0500

    Enhance upload middleware to support additional file formats and set public ID based on original file name for improved file management.

--- SERVER/middlewares/upload.middleware.js
+++ SERVER/middlewares/upload.middleware.js
@@ -6,13 +6,18 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: async (req, file) => {
     const folder = req.body.uploadFolder || 'default_folder';
+
+    const originalName = file.originalname.split(".")[0]; 
+
     return {
       folder,
-      allowed_formats: ['jpg', 'png', 'jpeg', 'webp', 'gif', 'bmp'],
-      transformation: [{ width: 300, height: 300, crop: 'limit' }],
+      public_id: originalName,
+      resource_type: 'auto',
+      allowed_formats: ['jpg', 'png', 'jpeg', 'webp', 'gif', 'bmp', 'pdf', 'doc', 'docx'],
     };
   },
 });
+
 
 const upload = multer({ storage });
 
