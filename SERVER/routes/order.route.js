dd2c0960e63acb801c926e38eaa67bf35f7cac7f
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-05-05 20:21:08 +0500

    Add review functionality for Test and Package orders in order.route.js

--- SERVER/routes/order.route.js
+++ SERVER/routes/order.route.js
@@ -13,6 +13,7 @@ import {
 
 import { isAuthenticated, isLabAdmin } from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/upload.middleware.js";
+import { addPackageReview, addTestReview } from "../controllers/testpackage.controller.js";
 
 const router = express.Router();
 
@@ -21,13 +22,15 @@ router.post("/create", isAuthenticated, createOrder);
 router.get("/user", isAuthenticated, getUserOrders);
 router.get("/all", isAuthenticated, isLabAdmin, getAllOrders);
 router.get("/lab", isAuthenticated, isLabAdmin, getLabOrders);
-
 // router.put("/:id/status", isAuthenticated, isLabAdmin,upload.single("report"), updateOrderStatus);
 router.put("/:id/update-status", isAuthenticated, isLabAdmin, upload.single("report"), updateOrderStatus);
 // router.put("/:id/update-status", isAuthenticated, isLabAdmin, updateOrderStatus);
 router.put("/:id/cancel", isAuthenticated, cancelOrder);
 router.delete("/:id", isAuthenticated, isLabAdmin, deleteOrder);
-
 router.get("/:id", isAuthenticated, getOrderById);
+router.post("/review/:id", isAuthenticated, addTestReview);
+router.post("/review/:id", isAuthenticated, addPackageReview);
+
+
 
 export default router;
