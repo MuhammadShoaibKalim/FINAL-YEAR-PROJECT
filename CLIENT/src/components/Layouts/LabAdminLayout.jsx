ee458b3a2cbe52344d017c3ae475686a04d715fa
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-05-07 01:30:07 +0500

    Add LabAdminLayout component to structure Lab Admin pages; includes Header, Footer, and Outlet for nested routing.

---
+++ CLIENT/src/components/Layouts/LabAdminLayout.jsx
@@ -0,0 +1,17 @@
+import Header from "../Headers/Header";
+import Footer from "../Headers/Footer";
+import { Outlet } from "react-router-dom";
+
+const LabAdminLayout = () => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default LabAdminLayout;
