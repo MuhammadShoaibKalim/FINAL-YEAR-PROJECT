3d868acb83f1af676375de9dfb40e9dd054f9b55
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-05-09 17:08:06 +0500

    Enhance order.route.js with new routes for lab order management. Add uploadReport, updateLabOrderStatus, and cancelLabOrder endpoints, ensuring proper authentication and lab admin access. Update existing status update route to use POST for report uploads.

--- SERVER/routes/order.route.js
+++ SERVER/routes/order.route.js
@@ -9,6 +9,9 @@ import {
   deleteOrder,
   cancelOrder,
   getOrderById,
+  updateLabOrderStatus,
+  cancelLabOrder,
+  uploadReport
 } from "../controllers/order.controller.js";
 
 import { isAuthenticated, isLabAdmin } from "../middlewares/auth.middleware.js";
@@ -22,15 +25,14 @@ router.post("/create", isAuthenticated, createOrder);
 router.get("/user", isAuthenticated, getUserOrders);
 router.get("/all", isAuthenticated, isLabAdmin, getAllOrders);
 router.get("/lab", isAuthenticated, isLabAdmin, getLabOrders);
-// router.put("/:id/status", isAuthenticated, isLabAdmin,upload.single("report"), updateOrderStatus);
+router.post("/:id/upload-report", isAuthenticated, isLabAdmin, upload.single("report"), uploadReport);
 router.put("/:id/update-status", isAuthenticated, isLabAdmin, upload.single("report"), updateOrderStatus);
-// router.put("/:id/update-status", isAuthenticated, isLabAdmin, updateOrderStatus);
 router.put("/:id/cancel", isAuthenticated, cancelOrder);
 router.delete("/:id", isAuthenticated, isLabAdmin, deleteOrder);
 router.get("/:id", isAuthenticated, getOrderById);
 router.post("/review/:id", isAuthenticated, addTestReview);
 router.post("/review/:id", isAuthenticated, addPackageReview);
-
-
+router.put("/:orderId/lab/:labId/status", isAuthenticated, isLabAdmin, updateLabOrderStatus);
+router.put("/:orderId/cancel/:labId", isAuthenticated, cancelLabOrder);
 
 export default router;
