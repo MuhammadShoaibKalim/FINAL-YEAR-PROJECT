9551a252be57be2f46384a1c48853bf92b5c4ed9
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-05-09 06:29:37 +0500

    Refactor vite.config.js to improve proxy configuration by adding WebSocket support and enhancing formatting for better readability.

--- CLIENT/vite.config.js
+++ CLIENT/vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
-  proxy: {
-    '/api': {
-      target: 'http://localhost:7000',
-      changeOrigin: true,
-      secure: false,
-      historyApiFallback: true, 
-    },
-  },
-},
-
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7000', 
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    }
+  }
 })
