57b5219dac798329132200558fe33f33f4c6d700
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-05-11 21:36:09 +0500

    Update vite.config.js

--- CLIENT/vite.config.js
+++ CLIENT/vite.config.js
@@ -1,17 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:7000', 
+        target: 'http://localhost:7000',
         changeOrigin: true,
         secure: false,
-        // ws: true,
+        ws: true,
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
   }
 })
